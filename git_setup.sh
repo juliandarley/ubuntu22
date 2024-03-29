a70adcbb6d023b06371e7b32fec9e37041c82dba
Julian Darley <julian@merlin-ai.com>
2024-03-29 10:24:37 +0000

    add instructions for how to check git params separately

--- git_setup.sh
+++ git_setup.sh
@@ -2,7 +2,13 @@
 
 # Load the .env file
 if [ -f ".env" ]; then
-    export $(cat .env | xargs)
+    while IFS='=' read -r key value
+    do
+        # Remove quotes which may be around the value
+        value=${value%\"}
+        value=${value#\"}
+        export "$key=$value"
+    done < ".env"
 else 
     echo ".env file not found. Please ensure it exists in the same directory as this script."
     exit 1
@@ -25,3 +31,9 @@ git config --global init.defaultBranch main
 
 echo "Git has been configured with username: '$NAME', email: '$EMAIL', and default branch name: 'main'."
 
+###--------------------###
+# to check that git params have changed the way you want, use:
+# git config --global user.name
+# git config --global user.email
+# if you have different names for specific projects, remove the flag --global
+###--------------------###
